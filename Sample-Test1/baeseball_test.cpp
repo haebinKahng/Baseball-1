ffe49c5c92551a0e8837df359753e914f05977aa
Minyoung Kang <haebin1223.myk@gmail.com>
2024-05-30 15:24:34 +0900

    TDD Cycle 3

--- Sample-Test1/baeseball_test.cpp
+++ Sample-Test1/baeseball_test.cpp
@@ -3,10 +3,10 @@
 
 class BaseballFixture : public testing::Test {
 public:
-	Baseball game;
+	Baseball game{ "123" };
 	void assertIllegalArgument(string guessNumber) {
 		try {
-			game.guess(string("12s"));
+			game.guess(string(guessNumber));
 			FAIL();
 		}
 		catch (exception e) {
@@ -18,4 +18,13 @@ public:
 TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
 	assertIllegalArgument("12");
 	assertIllegalArgument("12s");
+	assertIllegalArgument("121");
+}
+
+TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber) {
+	GuessResult result = game.guess("123");
+
+	EXPECT_TRUE(result.solved);
+	EXPECT_EQ(3, result.strikes);
+	EXPECT_EQ(0, result.balls);
 }
--- baseball/baseball.cpp
+++ baseball/baseball.cpp
@@ -1,12 +1,34 @@
 #include <stdexcept>
 using namespace std;
 
+struct GuessResult {
+	bool solved;
+	int strikes;
+	int balls;
+};
+
 class Baseball {
 public:
-	void guess(const string& guessNumber) {
-		if(guessNumber.length() != 3) throw length_error("Must be three letters.");
-		for (char ch : guessNumber) {
-			if (ch < '0' || ch > '9') throw invalid_argument("Must be number");
-		}
+	explicit Baseball(const string & question) : question(question) {}
+	bool isDuplicatedNumber(const string& guessNumber) {
+		if (guessNumber[0] == guessNumber[1]) return true;
+		if (guessNumber[0] == guessNumber[2]) return true;
+		if (guessNumber[1] == guessNumber[2]) return true;
+		return false;
 	}
+	void assertIllegalArgument(const string& guessNumber) {
+		if (guessNumber.length() != 3) throw length_error("Must be three letters.");
+		for (char ch : guessNumber) {
+			if (ch >= '0' && ch <= '9') continue;
+			throw invalid_argument("Must be number");
+		}
+		if (isDuplicatedNumber(guessNumber)) throw invalid_argument("Must not have same number");
+	}
+	GuessResult guess(const string& guessNumber) {
+		assertIllegalArgument(guessNumber);
+		if(guessNumber == question) return { true, 3, 0 };
+		return { false, 0, 0 };
+	}
+private:
+	string question;
 };
